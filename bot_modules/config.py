3e5760c5cd2735b0b14da79dfbc010061f839446
Lucas Toschi <lt.toschi@gmail.com>
2022-01-07 20:44:42 -0300

    Removing old code

--- bot_modules/config.py
+++ bot_modules/config.py
@@ -1,17 +1,10 @@
-#TODO How to deal with import secrets here?
-import secrets
-
 import os
-from bot_modules.notion_modules import NotionWorkspace
 from discord.ext.commands import Bot
 
 #Notion configurations
 botModulesPath = os.path.dirname(__file__)
 jsonDatabasesPath = botModulesPath + "/databases"
 jsonDatabasesRegisteredNotionPath = jsonDatabasesPath + "/registered_notion.json"
-
-#TODO Remove this later
-personalWorkspace = NotionWorkspace.NotionWorkspace(secrets.NotionKEY)
 
 #Bot configurations
 #TODO Elaborate a cool description for the bot.
