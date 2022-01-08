cc3a94349defaa9532d39aac595442fa7126f066
Lucas Toschi <lt.toschi@gmail.com>
2022-01-07 22:33:52 -0300

    Guild and DM messages are differenciable

--- bot_modules/eventsDef.py
+++ bot_modules/eventsDef.py
@@ -2,7 +2,13 @@ from bot_modules.config import *
 
 @sid.event
 async def on_message(message):
-    if message.content == 'test':
-        await message.channel.send("Testing 1 2 3!")
-    
+    if message.guild:
+        #The message comes from a server.
+        print("Guild")
+        if message.content == 'test':
+            await message.channel.send("Testing 1 2 3!")
+    else:
+        #The message comes from a DM.
+        print("DM")
+
     await sid.process_commands(message)
