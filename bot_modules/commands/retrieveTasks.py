54160b2e5021684ed9f988ea29a90812e8e643b7
Lucas Toschi <lt.toschi@gmail.com>
2022-01-09 22:07:45 -0300

    Implements readJSONFileAsDict in retrieveTasks command

--- bot_modules/commands/retrieveTasks.py
+++ bot_modules/commands/retrieveTasks.py
@@ -3,6 +3,7 @@
 
 import json
 from bot_modules.config import jsonDatabasesRegisteredNotionPath
+from bot_modules.utils import readJSONFileAsDict
 
 from bot_modules.notion_modules import NotionWorkspace
 from bot_modules.notion_modules import NotionDatabase
@@ -10,34 +11,36 @@ from bot_modules.notion_modules import NotionDatabase
 #TODO Defines secretToken keyword in config file.
 #TODO Defines id keyword in config file.
 #TODO Defines database keyword in config file.
-#TODO Treatment when some info not match the register.
 async def retrieveTasks(context, databaseAlias):
-    registers = {}
-    with open(jsonDatabasesRegisteredNotionPath, "r") as openfile:
-        try:
-            registers = json.load(openfile)
-        except json.decoder.JSONDecodeError:
-            pass
+    registers = readJSONFileAsDict(jsonDatabasesRegisteredNotionPath)
 
-    #TODO Maybe there is a more elegant way to do this.
-    #Reads the .json file
-    targetWorkpaceData = {}
-    targetDatabaseData = {}
-    for register in registers:
-        #Gets all the databases in a workspace.
-        workspace = registers[register]
-        databases = workspace["databases"]
+    if registers is not None:
+        #Reads the .json file
+        targetWorkpaceData = {}
+        targetDatabaseData = {}
+        databaseFound = False
+        for register in registers:
+            #Gets all the databases in a workspace.
+            workspace = registers[register]
+            databases = workspace["databases"]
 
-        for database in databases:
-            #For a single database, checks if it was already registered.
-            if(database == databaseAlias):
-                targetWorkpaceData = {k: workspace[k] for k in set(list(workspace)) - set(["databases"])}
-                targetDatabaseData = databases[database]
-                break
+            for database in databases:
+                #For a single database, checks if it was already registered.
+                if(database == databaseAlias):
+                    targetWorkpaceData = {k: workspace[k] for k in set(list(workspace)) - set(["databases"])}
+                    targetDatabaseData = databases[database]
+                    databaseFound = True
+                    break
 
-    workspaceObject = NotionWorkspace.NotionWorkspace(targetWorkpaceData["secretToken"])
-    databaseObject = workspaceObject.addDatabase(databaseAlias, targetDatabaseData["id"])
-    
-    formattedTable = databaseObject.getDatabaseDataToTablePrint()
+        if databaseFound:
+            workspaceObject = NotionWorkspace.NotionWorkspace(targetWorkpaceData["secretToken"])
+            databaseObject = workspaceObject.addDatabase(databaseAlias, targetDatabaseData["id"])
+            
+            formattedTable = databaseObject.getDatabaseDataToTablePrint()
 
-    await context.send(formattedTable)
+            await context.send(formattedTable)
+        
+        else:
+            await context.send("Database informado não foi cadastrado ou tem nome inválido!")
+    else:
+        await context.send("Não há databases cadastrados!")
