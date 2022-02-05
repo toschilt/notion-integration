7e912b7cb5e4cf7371b836d93e8fdaf551f0b563
toschilt <lt.toschi@gmail.com>
2022-02-05 14:30:43 -0300

    Config file creates databases and user JSON files if they not exist.

--- bot_modules/config.py
+++ bot_modules/config.py
@@ -2,7 +2,8 @@ import json
 import os
 from discord.ext.commands import Bot
 
-#Path basic configuration
+
+#----- BASIC PATH CONFIGURATION -----
 botModulesPath = os.path.dirname(__file__)
 jsonDatabasesPath = botModulesPath + "/databases"
 
@@ -10,11 +11,25 @@ jsonDatabasesPath = botModulesPath + "/databases"
 if not os.path.exists(jsonDatabasesPath):
     os.mkdir(jsonDatabasesPath)
 
-#Notion configurations
+
+
+#----- NOTION CONFIGURATIONS -----
 jsonDatabasesRegisteredNotionPath = jsonDatabasesPath + "/registered_notion.json"
 
-#Bot configurations
+#Check if the JSON database file exists. If not, create it.
+if not os.path.exists(jsonDatabasesRegisteredNotionPath):
+    with open(jsonDatabasesRegisteredNotionPath, 'w'):
+        pass
+
+
+
+#----- BOT CONFIGURATIONS -----
 jsonUsersRegisteredPath = jsonDatabasesPath + "/users.json"
+
+#Check if the JSON user file exists. If not, create it.
+if not os.path.exists(jsonUsersRegisteredPath):
+    with open(jsonUsersRegisteredPath, 'w'):
+        pass
 
 #TODO Elaborate a cool description for the bot.
 sid = Bot(command_prefix = "!",
